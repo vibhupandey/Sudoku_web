0b45cfb3b55daca06fe64a5ea155cf7ac1f7dfe6
Vibhu Pandey <vibhu.pandey@kdi.kongsberg.com>
2019-10-20 17:46:35 +0530

    rearraged the classes

---
+++ Suduko/BusinessLogic/SudokuInitializer.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Suduko.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Suduko.BusinessLogic
+{
+    public class SudokuInitializer
+    { 
+        public int[] FetchDatafromFile(string filePath)
+        {
+            string text = System.IO.File.ReadAllText(filePath);
+            text = text.Replace("\r\n", "");
+            text = text.Replace("{", "").Replace("}", "");
+            text = text.Trim();
+            int[] input = text.Split(',').Select(x => int.Parse(x)).ToArray<int>();
+            return input;
+        }
+
+        public SudukoModel ConvertToSudocoTable(int[] ProblemArray)
+        {
+            var data = new SudukoModel();
+            data.SudData = new int[9][];//Row Declaration.  
+            for (var i = 0; i < data.SudData.Length; i++)
+            {
+                data.SudData[i] = new int[9];//Column Declaration
+            }
+            Func<int, int, int> FindCurrentCell = (Row, Column) => (Row * 9) + Column;
+            //Push the values into the array.
+            for (var i = 0; i < data.SudData.Length; i++)
+            {
+                for (var j = 0; j < data.SudData.Length; j++)
+                {
+                    data.SudData[i][j] = ProblemArray[FindCurrentCell(i, j)];
+                }
+            }
+            return data;
+        }
+        public List<SelectListItem> getAllSudukoFiles(string selectedValue)
+        {
+            List<SelectListItem> ObjList = new List<SelectListItem>()
+            {
+            };
+            string[] Documents = System.IO.Directory.GetFiles("./SudokoData/");
+
+            for (int i = 0; i < Documents.Length; i++)
+            {
+                SelectListItem lstobj = null;
+
+                if (i == 0)
+                {
+                    if (string.IsNullOrEmpty(selectedValue))
+                    {
+                        lstobj = new SelectListItem { Text = Path.GetFileName(Documents[i].ToString()), Value = Documents[i].ToString(), Selected = true };
+                    }
+                }
+                if (Documents[i].ToString() == selectedValue)
+                {
+                    lstobj = new SelectListItem { Text = Path.GetFileName(Documents[i].ToString()), Value = Documents[i].ToString(), Selected = true };
+                }
+                else
+                {
+                    lstobj = new SelectListItem { Text = Path.GetFileName(Documents[i].ToString()), Value = Documents[i].ToString() };
+                }
+                ObjList.Add(lstobj);
+            }
+            return ObjList;
+
+        }
+
+    }
+}
--- Suduko/Controllers/HomeController.cs
+++ Suduko/Controllers/HomeController.cs
@@ -9,115 +9,52 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using Suduko.BusinessLogic;
 using Suduko.Models;  
 
 namespace Suduko.Controllers
 {
     public class HomeController : Controller
     {
-        private SudukoModel dataq = new SudukoModel();
-        private int a = 0;
+        private SudukoModel _sudukoModel = new SudukoModel();
+        private SudokuInitializer _sudokuInitializer = new SudokuInitializer(); 
         public IActionResult Index(String  id , string session)
         {
             if (string.IsNullOrEmpty(id))
             {
-                var getfile = getAllSudukoFiles(""); 
+                var getfile = _sudokuInitializer.getAllSudukoFiles(""); 
                 ViewBag.Locations = getfile;
-                dataq = ConvertToSudocoTable(FetchDatafromFile(getfile[0].Value)); 
+                _sudukoModel = _sudokuInitializer.ConvertToSudocoTable(_sudokuInitializer.FetchDatafromFile(getfile[0].Value)); 
                 
             }
             else  
             { 
-                var getfile = getAllSudukoFiles(HttpUtility.UrlDecode(session));
-                 
+                var getfile = _sudokuInitializer.getAllSudukoFiles(HttpUtility.UrlDecode(session));                 
                 ViewBag.Locations = getfile;
-                dataq = JsonConvert.DeserializeObject<SudukoModel>(id)  ;
+                _sudukoModel = JsonConvert.DeserializeObject<SudukoModel>(id)  ;
             }
-                return View(dataq);
+          return View(_sudukoModel);
         }
 
-        [NonAction]
-        protected int [] FetchDatafromFile(string filePath)
-        { 
-            string text = System.IO.File.ReadAllText(filePath);
-            text = text.Replace("\r\n", ""); 
-            text = text.Replace("{", "").Replace("}", "");
-            text = text.Trim();
-            int [] input = text.Split(',').Select(x => int.Parse(x)).ToArray<int>();
-            return input;
-        }
-        [NonAction]
-        protected SudukoModel ConvertToSudocoTable(int [] ProblemArray)
-        {
-            var data = new SudukoModel();
-            data.SudData = new int[9][];//Row Declaration.  
-            for (var i = 0; i < data.SudData.Length; i++)
-            {
-                data.SudData[i] = new int[9];//Column Declaration
-            }
-            Func<int, int, int> FindCurrentCell = (Row, Column) => (Row * 9) + Column;
-            //Push the values into the array.
-            for (var i = 0; i < data.SudData.Length; i++)
-            {
-                for (var j = 0; j < data.SudData.Length; j++)
-                {
-                    data.SudData[i][j] = ProblemArray[FindCurrentCell(i, j)];
-                }
-            }
-            return data;
-        } 
+        
         [HttpPost]
         public ActionResult Solve(SudukoModel data)
         { 
             Helper obj = new Helper(data); 
-            dataq = null;
-            dataq = obj.SolveSudoko(); 
-            //return RedirectToAction("PopulateResult", dataresult);
-            return RedirectToAction("Index",new { id= JsonConvert.SerializeObject(dataq), session = "" });
+           _sudukoModel = obj.SolveSudoko(); 
+           return RedirectToAction("Index",new { id= JsonConvert.SerializeObject(_sudukoModel), session = "" });
         } 
         public IActionResult indexchange(string value)
         {
-            dataq=ConvertToSudocoTable(FetchDatafromFile(value));
-            HttpContext.Session.SetString("indexchanged", value);
-            return RedirectToAction("Index", new { id = JsonConvert.SerializeObject(dataq), session=value });
+            _sudukoModel= _sudokuInitializer.ConvertToSudocoTable(_sudokuInitializer.FetchDatafromFile(value)); 
+            return RedirectToAction("Index", new { id = JsonConvert.SerializeObject(_sudukoModel), session=value });
         }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        private List<SelectListItem> getAllSudukoFiles( string  q)
-        {
-            List<SelectListItem> ObjList = new List<SelectListItem>()
-            { 
-            };
-            string[] Documents = System.IO.Directory.GetFiles("./SudokoData/");
-
-            for (int i = 0; i < Documents.Length; i++)
-            {
-                SelectListItem t = null;
-
-                if (i == 0)
-                {
-                    if (string.IsNullOrEmpty(q))
-                    {
-                        t = new SelectListItem { Text = Path.GetFileName(Documents[i].ToString()), Value = Documents[i].ToString(), Selected = true };
-                    }
-                }
-                if (Documents[i].ToString() == q)
-                {
-                    t = new SelectListItem {  Text =Path.GetFileName(Documents[i].ToString()), Value = Documents[i].ToString(), Selected = true};
-                }
-                else
-                {
-                    t = new SelectListItem { Text = Path.GetFileName(Documents[i].ToString()), Value = Documents[i].ToString() };
-                }
-                ObjList.Add(t);
-            } 
-            return ObjList;
-
-        }
-
+       
 
     }
 }
